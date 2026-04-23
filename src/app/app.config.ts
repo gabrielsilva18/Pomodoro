40a74e0bef4a086a08d9c2c17ba89d504cbd5ff7
Gabriel <gabriellealmaio1@gmail.com>
2026-04-23 13:10:50 -0300

    chore(config): ajustar bootstrap e tsconfig
    - Organiza o bootstrap e os providers da aplicação.
    - Define rootDir no tsconfig para padronizar a resolução de paths.
    - Pequenos ajustes de legibilidade.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,9 +4,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+// Configurações compartilhadas da aplicação.
+// Aqui configuramos o roteador e ativamos a hidratação do cliente para SSR.
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes),
+    provideClientHydration(withEventReplay())
   ]
 };
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
+// Ponto de entrada do aplicativo Angular.
+// O `bootstrapApplication` inicia o componente raiz `App` usando a configuração definida em appConfig.
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
